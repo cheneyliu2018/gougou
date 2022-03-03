843b2719a1c2388279fb6bfccc5f7faaf862dcef
baiyan <38801255+cheneyliu2018@users.noreply.github.com>
2022-03-03 16:49:01 +0800

    京东极速版金币

--- jd_speed_signfaker.js
+++ jd_speed_signfaker.js
@@ -14,10 +14,10 @@
 
 ================Loon==============
 [Script]
-cron "21 3,8 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_speed_sign.js,tag=京东极速版
+cron "21 3,8 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_speed_sign.js,tag=京东极速版金币
 
 ===============Surge=================
-京东极速版 = type=cron,cronexp="18 5,8,12,17,22 * * *",wake-system=1,timeout=33600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_speed_sign.js
+京东极速版金币 = type=cron,cronexp="18 5,8,12,17,22 * * *",wake-system=1,timeout=33600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_speed_sign.js
 
 ============小火箭=========
 京东极速版 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_speed_sign.js, cronexpr="18 5,8,12,17,22 * * *", timeout=33600, enable=true
