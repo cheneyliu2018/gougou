c92cf530eb797bb5ed4122df1e699bfb43199850
baiyan <38801255+cheneyliu2018@users.noreply.github.com>
2022-03-03 16:43:19 +0800

    Update jd_syj.js

--- jd_syj.js
+++ jd_syj.js
@@ -15,10 +15,10 @@ Last Modified time: 2022-2-8
 cron "10 0,7,23 * * *" script-path=jd_syj.js, tag=赚京豆-瓜分京豆
 
 ===============Surge=================
-赚京豆-瓜分京豆 = type=cron,cronexp="10 0,7,23 * * *",wake-system=1,timeout=3600,script-path=jd_syj.js
+赚京豆-瓜分京豆 = type=cron,cronexp="10 5,7 * * *",wake-system=1,timeout=3600,script-path=jd_syj.js
 
 ============小火箭=========
-赚京豆-瓜分京豆 = type=cron,script-path=jd_syj.js, cronexpr="10 0,7,23 * * *", timeout=3600, enable=true
+赚京豆-瓜分京豆 = type=cron,script-path=jd_syj.js, cronexpr="10 5,7 * * *", timeout=3600, enable=true
  */
 const $ = new Env('赚京豆-瓜分京豆');
 $.appId = 'dde2b';
